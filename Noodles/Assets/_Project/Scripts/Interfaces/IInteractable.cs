6c4976e3d834dd11184cb8ab68e2621ec443734f
MH <michael.hardwicke92@gmail.com>
2026-04-03 15:48:36 -0600

    Interfaces

---
+++ Noodles/Assets/_Project/Scripts/Interfaces/IInteractable.cs
@@ -0,0 +1,9 @@
+namespace _Project.Scripts.Interfaces
+{
+    public interface IInteractable
+    {
+        public void OnHoverIn();
+        public void OnInteract();
+        public void OnHoverOff();
+    }
+}
